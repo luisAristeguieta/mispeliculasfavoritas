dde4230e2a2dd8be5462c38bf7e45e8f7bda1f19
luisAristeguieta <luisjimenezgx@gmail.com>
2026-06-10 21:58:57 -0400

    feat(pelicula): add movie posters images and improve layout with flexbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 
 
-function Pelicula({ titulo }) {
+function Pelicula({ titulo, imagen }) {
     
    const [esFavorita, setEsFavorita] = useState(false);
 
@@ -15,9 +15,26 @@ function Pelicula({ titulo }) {
             margin: '5px',
             cursor: 'pointer',
             backgroundColor: esFavorita ? '#f9f0b3' : '#f5f5f5',
-            borderRadius: '5px'
+            borderRadius: '5px',
+            display: 'flex',
+            alignItems: 'center',
+            gap: '15px'
         }}>
-            <p>{titulo} {esFavorita ? '⭐' : ''}</p>
+          {/* Imagen de la película */}
+            {imagen && (
+                <img 
+                    src={imagen} 
+                    alt={titulo}
+                    style={{
+                        width: '50px',
+                        height: '70px',
+                        objectFit: 'cover',
+                        borderRadius: '5px'
+                    }}
+                />
+            )}
+            <p style={{ flex: 1, margin: 0 }}>
+              {titulo} {esFavorita ? '⭐' : ''}</p>
         </div>
     );
 }
@@ -26,17 +43,17 @@ function Pelicula({ titulo }) {
 function App() {
   // Array de peliculas (strings)
     const peliculas = [
-        'The Sheep Detectives',
-        'Obsesión',
-        'Hoppers',
-        'The Housemaid'
+        { titulo: 'The Sheep Detectives', imagen: '/2.jpg' },
+        { titulo: 'Obsesión', imagen: '/3.jpg' },
+        { titulo: 'Hoppers', imagen: '/4.jpeg' },
+        { titulo: 'The Housemaid', imagen: '/1.jpg' }
     ];
 
   return (
-    <div>
-      <h1>Mis Películas Favoritas</h1>
+    <div style={{ maxWidth: '600px', margin: '0 auto', padding: '20px' }}>
+      <h1 style={{ textAlign: 'center' }} >Mis Películas Favoritas</h1>
               {peliculas.map((pelicula, indice) => (
-                <Pelicula key={indice} titulo={pelicula} />
+                <Pelicula key={indice} titulo={pelicula.titulo} imagen={pelicula.imagen} />
             ))}
     </div>
   )
