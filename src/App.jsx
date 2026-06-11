b89b4331afc64ea8a601e4eb4544cebb9c14dc8e
luisAristeguieta <luisjimenezgx@gmail.com>
2026-06-10 20:05:29 -0400

    feat(pelicula): add Pelicula component that receives titulo prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 
+
+function Pelicula({ titulo }) {
+    return (
+        <div>
+            <p>{titulo}</p>
+        </div>
+    );
+}
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
       <h1>Mis Películas Favoritas</h1>
+      <Pelicula titulo="The Sheep Detectives" />
+      <Pelicula titulo="Obsesión" />
+      <Pelicula titulo="Hoppers" />
+      <Pelicula titulo="The Housemaid" />
     </div>
   )
 }
+
+
 
 export default App
